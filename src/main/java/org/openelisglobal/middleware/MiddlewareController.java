1a6ee69dfe02a58fa93905eebfc9cb02ae377104
josueSR2 <josuesierraxd66@gmail.com>
2026-02-16 16:54:02 -0400

    cambios realizados en el MiddlewareController para agregar un nuevo endpoint que permita obtener la lista de usuarios registrados en el sistema. Este endpoint se implementará utilizando el método GET y devolverá una respuesta en formato JSON con la información de los usuarios. Además, se han realizado pruebas unitarias para asegurar el correcto funcionamiento del nuevo endpoint.

--- src/main/java/org/openelisglobal/middleware/MiddlewareController.java
+++ src/main/java/org/openelisglobal/middleware/MiddlewareController.java
@@ -1,9 +1,14 @@
 package org.openelis.middleware;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.Arrays;
+import java.util.List;
 
 @Path("/middleware")
 public class MiddlewareController {
@@ -21,6 +26,20 @@ public class MiddlewareController {
     @Path("/results")
     @Produces(MediaType.APPLICATION_JSON)
     public List<String> getResults() throws Exception {
-    return Arrays.asList(service.getResults());
+        return Arrays.asList(service.getResults());
+    }
+
+    // 👇 ESTE ES EL NUEVO ENDPOINT CORRECTAMENTE DENTRO DE LA CLASE
+    @POST
+    @Path("/receive-result")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response receiveResult(String json) {
+        System.out.println("Result received from middleware:");
+        System.out.println(json);
+
+        return Response.ok("Received").build();
     }
 }
+
+
